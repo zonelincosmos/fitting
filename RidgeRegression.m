dfcdbbe8de1390fef01bcae556f4405a8191ab23
zonelincosmos <36846547+zonelincosmos@users.noreply.github.com>
2024-09-22 20:53:55 +0800

    Add files via upload

---
+++ RidgeRegression.m
@@ -0,0 +1,72 @@
+%% Ridge Regression
+% 20240922 
+clear all;clc;clear all;close all;
+
+%% Data Sets
+DataX = [0 : pi/5: 2*pi];
+DataY_Orig = sin(DataX);
+% DataY = DataY_Orig + randi([-10 10], 1, length(DataX))/40;
+DataY = [ 0.2250    0.3878    1.0761    1.2011    0.6378    0.1250  -0.6878   -0.9761   -1.1761   -0.6378   -0.0750 ];
+
+% plot(DataX, DataY, 'o-')
+% hold on
+% plot(DataX, DataY_Orig, 'o-')
+%% Test for Loop
+Cnt = 1;
+coeff = zeros(11,4);
+TestOrder = 11;
+TestLambda = 0;
+for TestOrder = 5:2:11
+% for TestLambda = 0:0.02:0.06
+
+%% Polynomial Fitting
+% y =c1*x + c2*x^2 + c3*x^3 +c4*x^4 + .....
+PolyOrder = TestOrder;
+lambda = TestLambda;
+A_Col = DataX.';
+b_Col = DataY.';
+A = A_Col;
+for order = 2:1:PolyOrder
+    A = [A A_Col.^order];
+end
+
+%coeff = A\b_Col;
+coeff(1:PolyOrder,Cnt) = inv(A'*A + lambda*eye(PolyOrder))*(A'*b_Col)
+
+%% Predict
+Input = [0 : pi/50: 2*pi].';
+PredictA = Input;
+for order = 2:1:PolyOrder
+    PredictA = [PredictA Input.^order];
+end
+PredictedY = PredictA*coeff(1:PolyOrder,Cnt);
+residue(Cnt) = sum(abs(PredictedY - sin(Input)))
+residue2(Cnt) = sum(abs(A*coeff(1:PolyOrder,Cnt) - DataY.'))
+
+subplot(2,2,Cnt)
+plot(Input, PredictedY, '.-', 'LineWidth', 2)
+hold on
+plot(Input, sin(Input), '--', 'LineWidth', 1, 'Color', 'black')
+hold on
+plot(DataX, DataY, '.', 'LineWidth', 1, 'MarkerSize',18, 'MarkerEdgeColor','red')
+grid on
+
+%% Test for Loop
+Cnt =  Cnt + 1;
+end
+
+%% Plot Residule
+figure
+subplot(3,1,1)
+plot(residue,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
+grid on
+
+subplot(3,1,2)
+plot(residue2,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
+grid on
+
+
+%% Plot Coefficient
+subplot(3,1,3)
+plot(coeff,  '.-', 'LineWidth', 2, 'MarkerSize',15, 'MarkerEdgeColor','black')
+grid on
