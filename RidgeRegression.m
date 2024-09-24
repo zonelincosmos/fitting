d2c7c55cb6cdfe3ad4ae8ebcbdb2633a9d1fd63d
zonelincosmos <36846547+zonelincosmos@users.noreply.github.com>
2024-09-25 01:23:40 +0800

    Add files via upload

--- RidgeRegression.m
+++ RidgeRegression.m
@@ -13,11 +13,12 @@ DataY = [ 0.2250    0.3878    1.0761    1.2011    0.6378    0.1250  -0.6878   -0
 % plot(DataX, DataY_Orig, 'o-')
 %% Test for Loop
 Cnt = 1;
-coeff = zeros(11,4);
+coeff = zeros(11,6);
 TestOrder = 11;
 TestLambda = 0;
-for TestOrder = 5:2:11
-% for TestLambda = 0:0.02:0.06
+% for TestOrder = 1:2:11
+% for TestOrder = 9:2:15
+for TestLambda = 0:0.004:0.02
 
 %% Polynomial Fitting
 % y =c1*x + c2*x^2 + c3*x^3 +c4*x^4 + .....
@@ -43,7 +44,7 @@ PredictedY = PredictA*coeff(1:PolyOrder,Cnt);
 residue(Cnt) = sum(abs(PredictedY - sin(Input)))
 residue2(Cnt) = sum(abs(A*coeff(1:PolyOrder,Cnt) - DataY.'))
 
-subplot(2,2,Cnt)
+subplot(3,2,Cnt)
 plot(Input, PredictedY, '.-', 'LineWidth', 2)
 hold on
 plot(Input, sin(Input), '--', 'LineWidth', 1, 'Color', 'black')
@@ -58,15 +59,24 @@ end
 %% Plot Residule
 figure
 subplot(3,1,1)
-plot(residue,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
+plot([0:0.004:0.02], residue,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
 grid on
 
 subplot(3,1,2)
-plot(residue2,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
+plot([0:0.004:0.02], residue2,  '.-', 'LineWidth', 2, 'MarkerSize',25, 'MarkerEdgeColor','blue')
 grid on
 
 
-%% Plot Coefficient
+%% Plot Coefficient Abs (sweep lambda)
 subplot(3,1,3)
-plot(coeff,  '.-', 'LineWidth', 2, 'MarkerSize',15, 'MarkerEdgeColor','black')
+for cnt = 1:1:5
+    plot([0:0.004:0.02], abs(coeff(cnt, :)),  '.-', 'LineWidth', 2, 'MarkerSize',15, 'MarkerEdgeColor','black')
+    hold on
+end
 grid on
+
+%% Plot Coefficient Abs (sweep order)
+% subplot(3,1,3)
+% plot([1:2:11], abs(coeff(1, :)),  '.-', 'LineWidth', 2, 'MarkerSize',15, 'MarkerEdgeColor','black')
+% grid on
+
